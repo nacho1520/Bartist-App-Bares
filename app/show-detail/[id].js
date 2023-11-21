2873746299d692a5c1dbcb0f6a370290dc7fbaa1
Ignacio Lopez <ignaciolopestela12@gmail.com>
2023-11-21 13:54:50 -0300

    Show artist applied

--- app/show-detail/[id].js
+++ app/show-detail/[id].js
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import { ScrollView, View, SafeAreaView, ActivityIndicator } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { ScrollView, View, SafeAreaView, ActivityIndicator, FlatList } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
 
-import { ShowInfo, ShowTabs, ShowAbout, BulletPointsInfo, PrimaryBtn } from "../../components";
+import { ShowInfo, ShowTabs, ShowAbout, BulletPointsInfo, ArtistCard } from "../../components";
 import { COLORS } from "../../constants";
 import useFetch from "../../hooks/useFetch";
 
-const tabs = ["Descripción", "Información del show", "Detalles tecnicos", "Oferta"];
+const tabs = ["Descripción", "Información del show", "Detalles tecnicos", "Oferta", "Artistas aplicados"];
 
 const ShowDetail = () => {
     const [ activeTab, setActiveTab ] = useState(tabs[0]);
     const params = useLocalSearchParams();
+    const router = useRouter();
     const { data, isLoading, error } = useFetch(`/shows/${ params.id }`);
     
     const displayContent = (show) => {
@@ -37,6 +38,26 @@ const ShowDetail = () => {
                             labels={["Pago inicial", "% ventas de entrada", "Costos adicionales", "Alimentación y bebida", "Forma de pago"]}
                             data={[ '$' + show.charter , show.sellsPercent , show.additionalCosts , show.foodIncluded ? 'Incluida' : 'No incluida' , show.payMethod]}
                         />;
+            case "Artistas aplicados": 
+                return (
+                    <View style={{ padding: 16, marginTop: 20 }}>
+                        <FlatList 
+                            data={ show.artistApplied }
+                            renderItem={ ({ item }) => (
+                                <ArtistCard 
+                                    title={ item.name }
+                                    subTitle={ item.genres.split(',')[0] }
+                                    imageUrl={ item.profileImage }
+                                    handleNavigation={ () => { router.push(`/artist-profile/${ item._id }`) } }
+                                />
+                            )}
+                            keyExtractor={ item => item?.name }
+                            contentContainerStyle={{ columnGap: 16 }}
+                            horizontal
+                            showsHorizontalScrollIndicator={ false }
+                        />
+                    </View>
+                );
             default: 
                 break;
         }
